a547c12f5f48fbc32a1b32da1d9d6cb315eff538
Nikki Licea <nlicea@usc.edu>
2022-06-26 18:47:04 -0700

    Create Database Table and Save & Retrieval Endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,12 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
-from peewee import *
+from peewee import *  # abstracts away the need to manually create tables
+import datetime  # for TimelinePost
+from playhouse.shortcuts import model_to_dict # for post_time_line_post()
 
 load_dotenv()
 app = Flask(__name__)
-
-# add MySQL Database
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
-                     user=os.getenv("MYSQL_USER"),
-                     password=os.getenv("MYSQL_PASSWORD"),
-                     host=os.getenv("MYSQL_HOST"),
-                     port=3306)
-print(mydb)
-
 
 @app.route('/')
 def index():
@@ -31,3 +24,41 @@ def hobbies():
 def travel():
     return render_template('travel.html')
 
+# add MySQL Database
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+                     user=os.getenv("MYSQL_USER"),
+                     password=os.getenv("MYSQL_PASSWORD"),
+                     host=os.getenv("MYSQL_HOST"),
+                     port=3306)
+print(mydb)
+
+# ORM model/Create Database Table
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+# new POST route which adds a timeline post
+@app.route('/api/timeline_post', methods = ['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, emial=email, content=content)
+    return model_to_dict(timeline_post)
+
+# GET endpoint that retrieves all timeline posts ordered by created_at descending so the newest timeline posts are returned at the top.
+@app.route('/api/timeline_post', methods = ['GET'])
+def get_time_line_post():
+    return{
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
