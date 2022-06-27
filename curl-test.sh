4a0ce08c9be0b69d53ff683f0a6f3861aac0feb1
Nikki Licea <nlicea@usc.edu>
2022-06-27 02:02:13 -0700

    Remove duplicate data

--- curl-test.sh
+++ curl-test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # POST: create random timeline post using the POST endpoint 
-curl -X POST http://nikkilicea.duckdns.org:5000/api/timeline_post -d 'name=Nikki&email=nlicea@usc.edu&content=Testing my endpoints with curl!'{"content":"Testing my endpoints with curl.", "created_at":"Mon, 27 June 2022 01:41:08 PST", "email":"nlicea@usc.edu", "id":0, "name":"Nikki"}
+curl -X POST http://nikkilicea.duckdns.org:5000/api/timeline_post -d \
+'name=Nikki&email=nlicea@usc.edu&content=Testing my endpoints with curl!'
 
 # GET: check the GET endpoint to make sure POST was added
 curl http://nikkilicea.duckdns.org:5000/api/timeline_post
