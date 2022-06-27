ee11abaca4104b63bd341a459ce7948854b697d8
Nikki Licea <nlicea@usc.edu>
2022-06-27 01:49:03 -0700

    Create POST/GET Requests with curl

---
+++ curl-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# POST: create random timeline post using the POST endpoint 
+curl -X POST https://http://nikkilicea.duckdns.org:5000/api/timeline_post
+   -d 'name=Nikki&email=nlicea@usc.edu&content=Testing my endpoints with curl!'
+   {"content":"Testing my endpoints with curl.", "created_at":"Mon, 27 June 2022 01:41:08 PST",
+   "email":"nlicea@usc.edu", "id":0, "name":"Nikki"}
+
+# GET: check the GET endpoint to make sure POST was added
+curl https://http://nikkilicea.duckdns.org:5000/api/timeline_post
