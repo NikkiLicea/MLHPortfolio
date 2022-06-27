d4258c03acc0a7eed1a5382844c50b0cfcaf00aa
Nikki Licea <nlicea@usc.edu>
2022-06-27 01:56:22 -0700

    Update link

--- curl-test.sh
+++ curl-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # POST: create random timeline post using the POST endpoint 
-curl -X POST https://http://nikkilicea.duckdns.org:5000/api/timeline_post -d 'name=Nikki&email=nlicea@usc.edu&content=Testing my endpoints with curl!'{"content":"Testing my endpoints with curl.", "created_at":"Mon, 27 June 2022 01:41:08 PST", "email":"nlicea@usc.edu", "id":0, "name":"Nikki"}
+curl -X POST http://nikkilicea.duckdns.org:5000/api/timeline_post -d 'name=Nikki&email=nlicea@usc.edu&content=Testing my endpoints with curl!'{"content":"Testing my endpoints with curl.", "created_at":"Mon, 27 June 2022 01:41:08 PST", "email":"nlicea@usc.edu", "id":0, "name":"Nikki"}
 
 # GET: check the GET endpoint to make sure POST was added
-curl https://http://nikkilicea.duckdns.org:5000/api/timeline_post
+curl http://nikkilicea.duckdns.org:5000/api/timeline_post
