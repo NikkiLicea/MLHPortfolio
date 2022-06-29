986d37aad58ee0b2045fa092c060aab0e502215a
Nikki Licea <nlicea@usc.edu>
2022-06-28 21:01:23 -0700

    Load Table of Posts Draft

--- app/templates/timeline.js
+++ app/templates/timeline.js
@@ -1,10 +1,36 @@
-/* Sending the URL-encoded string as payload using Fetch */
+// api url
+const api_url = '/api/timeline_post'; 
+
 const form = document.getElementById('timeline_form');
+
+// Defining async function
+async function getapi(url) {
+
+	// Storing response
+	const response = await fetch('/api/timeline_post', {
+        method: 'POST',
+        body: form
+    });
+
+	// Storing data in form of JSON
+	var data = await response.json();
+	console.log(data);
+	if (response) {
+		hideloader();
+	}
+	show(data);
+}
+// Calling that async function
+getapi(api_url);
+
+/* Sending the URL-encoded string as payload using Fetch */
 form.addEventListener('submit', function(e) {
     // Prevent default behavior:
     e.preventDefault();
+
     // Convert formData object to URL-encoded string:
     const payload = new FormData(form);
+
     // Post the payload using Fetch:
     fetch('/api/timeline_post', {
         method: 'POST',
@@ -16,7 +42,7 @@ form.addEventListener('submit', function(e) {
         setTimeout(function(){
             window.location.reload();}, 100);
         })
-    })
+})
 
 // Function to define innerHTML for HTML table
 function show(data) {
