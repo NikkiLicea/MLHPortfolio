144c16044bfa73fe0883e4abf944dc9b53abf8a1
Nikki Licea <nlicea@usc.edu>
2022-06-29 01:07:16 -0700

    Update Timeline JS

--- app/static/javascript/timeline.js
+++ app/static/javascript/timeline.js
@@ -1,49 +1,50 @@
 // api url
-const api_url = '/api/timeline_post'; 
+// const api_url = '/api/timeline_post'; 
 
 const form = document.getElementById('timeline_form');
-
-
 // Defining async function
-async function getapi(url) {
+// async function getapi(url) {
 
-	// Storing response
-	const response = await fetch(url, {
-        method: 'POST',
-        body: form
-    });
-
-	// Storing data in form of JSON
-	var data = await response.json();
-	console.log(data);
-	if (response) {
-		hideloader();
-	}
-	show(data);
-}
-// Calling that async function
-getapi(api_url);
-
-/* Sending the URL-encoded string as payload using Fetch */
-// form.addEventListener('submit', function(e) {
-//     // Prevent default behavior:
-//     e.preventDefault();
-
-//     // Convert formData object to URL-encoded string:
-//     const payload = new FormData(form);
-
-//     // Post the payload using Fetch:
-//     fetch('/api/timeline_post', {
+// 	// Storing response
+// 	const response = await fetch(url, {
 //         method: 'POST',
 //         body: form
-//     })
-//     .then(res => res.json())
-//     .then(data => console.log(data))
-//     .finally(function () {
-//         setTimeout(function(){
-//             window.location.reload();}, 100);
-//         })
-// })
+//     });
+
+// 	// Storing data in form of JSON
+// 	var data = await response.json();
+// 	console.log(data);
+// 	if (response) {
+// 		hideloader();
+// 	}
+// 	show(data);
+// }
+// // Calling that async function
+// getapi(api_url);
+
+/* Sending the URL-encoded string as payload using Fetch */
+form.addEventListener('submit', function(e) {
+    // Prevent default behavior:
+    e.preventDefault();
+
+    // Convert formData object to URL-encoded string:
+    const payload = new FormData(form);
+
+    console.log([...payload])
+
+    // Post the payload using Fetch:
+    fetch('/api/timeline_post', {
+        method: 'POST',
+        body: payload
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+    .finally(function () {
+        setTimeout(function(){
+            window.location.reload();}, 100);
+        })
+})
 
 // Function to define innerHTML for HTML table
 function show(data) {
