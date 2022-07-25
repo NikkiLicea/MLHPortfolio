f579d0aab315fff570d34e00de0b3a9125f028b4
NikkiLicea <nikkilicea@yahoo.com>
2022-07-25 15:29:38 -0700

    Update response

--- tests/test_db.py
+++ tests/test_db.py
@@ -34,6 +34,5 @@ class TestTimelinePost(unittest.TestCase):
 		second_post = TimelinePost.create(name='Jane Doe', email='jame@example.com', content='Hello world, I\'m Jane!')
 		assert second_post.id == 2
 		
-# 		response = get_time_line_post()
-		response = TimelinePost.get_time_linePost()
+		response = TimelinePost.select().order_by(TimelinePost.created_at.desc())
 		assert len(response['timeline_posts']) == 2
