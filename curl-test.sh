e23309343c18b3a23dd77d38dea4e2443ead3751
NikkiLicea <nikkilicea@yahoo.com>
2022-07-29 23:54:06 -0700

    Delete curl-test.sh

--- curl-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# POST: create random timeline post using the POST endpoint 
-curl -X POST http://nikkilicea.duckdns.org:5000/api/timeline_post -d \
-'name=Nikki&email=nlicea@usc.edu&content=Testing my endpoints with curl!'
-
-# GET: check the GET endpoint to make sure POST was added
-curl http://nikkilicea.duckdns.org:5000/api/timeline_post
